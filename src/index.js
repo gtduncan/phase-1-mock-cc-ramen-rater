41c8b3d24cb30e402ca716ff82f332d318f4efcf
George Duncan <gtduncan@usc.edu>
2022-07-06 14:54:46 -0400

    commit tree 3030681c1faa1f333a6493a2d362dfc9729853e9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-// write your code here
+const ramenMenuDiv = document.getElementById('ramen-menu')
+const ramenImage = document.querySelector('.detail-image')
+const ramenName = document.querySelector('.name')
+const ramenRestaurant = document.querySelector('.restaurant')
+const ramenRating = document.querySelector('#rating-display')
+const ramenComment = document.querySelector('#comment-display')
+const ramenForm = document.querySelector('#new-ramen')
+
+function addImageClick(img, ramen)
+{
+    img.addEventListener("click", () => {
+        ramenImage.src = ramen.image
+        ramenName.innerText = ramen.name
+        ramenRestaurant.innerText = ramen.restaurant
+        ramenRating.innerText = ramen.rating
+        ramenComment.innerText = ramen.comment
+    })
+}
+
+function populateMenu(array)
+{
+    console.log(array)
+    for(let i=0; i<array.length; i++)
+    {
+        let newImg = document.createElement('img');
+        newImg.src = array[i].image
+        ramenMenuDiv.append(newImg);
+        addImageClick(newImg, array[i]);
+    }
+}
+
+function fetchRamen()
+{
+    return fetch('http://localhost:3000/ramens')
+    .then(res => res.json())
+    .then(function makeSite(data) {
+        populateMenu(data)
+    })
+}
+
+ramenForm.addEventListener('submit', function addNewRamen(event) {
+    event.preventDefault()
+    let newRamen = {};
+    newRamen['name'] = document.getElementById('new-name').value
+    newRamen['restaurant'] = document.getElementById('new-restaurant').value
+    newRamen['image'] = document.getElementById('new-image').value
+    newRamen['rating'] = document.getElementById('new-rating').value
+    newRamen['comment'] = document.getElementById('new-comment').value
+    console.log(newRamen)
+    let newImage = document.createElement('img')
+    ramenMenuDiv.append(newImage);
+    newImage.src = newRamen['image']
+    addImageClick(newImage, newRamen)
+})
+
+fetchRamen()
+
